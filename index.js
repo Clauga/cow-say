352850916a708b3e4d18732f6d94a6ab42af0e32
Claudia GUERRERO <117662080+Clauga@users.noreply.github.com>
2022-12-02 13:21:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const myInformation = require('./information');
 let cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text: `Hello, I'm ${myInformation.myName} from ${myInformation.myCampus} campus !`,
+    text: `Hello there, I'm ${myInformation.myName} from ${myInformation.myCampus} campus !`,
     e: "oO",
     T: "U "
 }));
